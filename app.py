6ded4510e658730199ae8e946acf2f25f31f2c12
DTsai2 <50611692+DTsai2@users.noreply.github.com>
2022-09-15 20:24:40 +0800

    Update app.py

--- app.py
+++ app.py
@@ -16,6 +16,38 @@ from linebot.models import *
 
 from g_search import g_search
 
+# Import the beautifulsoup 
+# and request libraries of python.
+import requests
+from bs4 import BeautifulSoup
+import re
+
+def g_search(query):
+    # Make two strings with default google search URL
+    # 'https://google.com/search?q=' and
+    # our customized search keyword.
+    # Concatenate them
+    text= query
+    url = 'https://www.google.com/search?q=' + text
+    #Fetch the URL data using requests.get(url),
+    # store it in a variable, request_result.
+    request_result=requests.get( url )
+  
+    # Creating soup from the fetched request
+    soup = BeautifulSoup(request_result.text,
+                         "lxml")
+    # Find all the a tags and get the text/href
+    tags = soup.find_all("a")
+    info = ""
+    for tag in tags:
+        href = re.search("https?://\S*&sa",tag["href"])
+        text = tag.getText()
+        if (href and text not in ["地圖","規劃路線","網站"]):
+            href = href.group(0)[:-3]
+            info = info + text + "\n" + href + "\n" + "-------------" + "\n"
+            
+    return info
+
 app = Flask(__name__)
 
 # Channel Access Token
@@ -53,58 +85,3 @@ import os
 if __name__ == "__main__":
     port = int(os.environ.get('PORT', 5000))
     app.run(host='0.0.0.0', port=port)
-
-
-# In[ ]:
-
-
-
-
-
-# In[ ]:
-
-
-
-
-
-# In[ ]:
-
-
-
-
-
-# In[ ]:
-
-
-
-
-
-# In[ ]:
-
-
-
-
-
-# In[ ]:
-
-
-
-
-
-# In[ ]:
-
-
-
-
-
-# In[ ]:
-
-
-
-
-
-# In[ ]:
-
-
-
-
