0e52b2e96f189e46cc7cea648dae5d8b85801dac
DTsai2 <50611692+DTsai2@users.noreply.github.com>
2022-09-20 20:14:59 +0800

    Update app.py

--- app.py
+++ app.py
@@ -14,6 +14,57 @@ from linebot.exceptions import (
 )
 from linebot.models import *
 
+# scrap package
+from selenium import webdriver
+from webdriver_manager.chrome import ChromeDriverManager
+from parsel import Selector
+import time
+
+
+
+#### scrap code #####
+# search results from google map
+def scrapping(key_word) :
+    key_food = '火鍋106'
+    key_place = key_word
+    driver = webdriver.Chrome(ChromeDriverManager().install())
+    url = 'https://www.google.com/maps/search/{0}+near+{1}'.format(key_food, key_place)
+    driver.get(url)
+    page_content = driver.page_source
+    response = Selector(page_content)
+
+    results = []
+
+    for el in response.xpath('//div[contains(@aria-label, "的搜尋結果")]/div/div[./a]'):
+        results.append({
+            'link': el.xpath('./a/@href').extract_first(''),
+            'title': el.xpath('./a/@aria-label').extract_first(''),
+            'type': el.xpath('div//div[contains(@class, "UaQhfb fontBodyMedium")]//div[contains(@class, "W4Efsd")]/following::div//span[text()="·"]/following::span/text()').extract_first(),
+            'rating': el.xpath('div//span[contains(@aria-hidden, "true")]/text()').extract_first(''),
+            'reviewsCount': el.xpath('div//span[contains(@aria-hidden, "true")]/following::text()').extract_first(''),
+            # 'service'
+            # response.xpath('//div[contains(@aria-label, "的搜尋結果")]/div//div[contains(@class, "UaQhfb fontBodyMedium")]').extract_first('')
+            'address': el.xpath('div//div[contains(@class, "UaQhfb fontBodyMedium")]//div[contains(@class, "W4Efsd")]/following::div//span[@jsan="0.aria-hidden"]/following::span/text()').extract_first(),
+            'status': el.xpath('div//div[contains(@class, "UaQhfb fontBodyMedium")]//div[contains(@class, "W4Efsd")]/following::div[3]//span[text()="·"]/following::span/text()').extract_first(),
+            'nextOpenTime': el.xpath('div//div[contains(@class, "UaQhfb fontBodyMedium")]//div[contains(@class, "W4Efsd")]/following::div[3]//span[text()="·"]/following::span[4]/text()').extract_first()
+        })
+
+    #print(results)
+
+    search_result = [a['title'] for a in results]
+    picked_result = search_result[3]
+    #target_url = [a['link'] for a in results if a['title'] == picked_result]
+    #driver.get(target_url[0])
+    #to refresh the browser
+    #driver.refresh()
+
+    # search results of related blogs
+    driver.quit()
+    # return
+    return(picked_result)
+
+
+#### app
 app = Flask(__name__)
 
 # Channel Access Token
@@ -41,9 +92,9 @@ def callback():
 @handler.add(MessageEvent, message=TextMessage)
 def handle_message(event):
     message = event.message.text
-    if "隨機找店家" in message :
+    if "結果" in message :
         carousel_message = TemplateSendMessage(
-        alt_text = "food_cate",
+        alt_text = "results",
         template = CarouselTemplate(
         columns=[
             CarouselColumn(
@@ -52,16 +103,34 @@ def handle_message(event):
             text = "請點選其中一個種類",
             actions = [
                 MessageAction(
-                label = "日式料理",
-                text = "日式料理"),
+                label = "台北市",
+                text = "台北市"),
                 MessageAction(
-                label = "中式料理",
-                text = "中式料理")
+                label = "台北市",
+                text = "台北市")
             ])
         ]))
         line_bot_api.reply_message(event.reply_token, carousel_message)
-    else:
-        line_bot_api.reply_message(event.reply_token, TextSendMessage(message))
+    elif message == "台北市":
+        carousel_message = TemplateSendMessage(
+        alt_text = "results",
+        template = CarouselTemplate(
+        columns=[
+            CarouselColumn(
+            thumbnail_image_url = "https://www.iberdrola.com/documents/20125/39904/real_food_746x419.jpg",
+            title = "台北市",#scrapping(event.message.text),
+            text = "台北市",#scrapping(event.message.text),
+            actions = [
+                MessageAction(
+                label = "台北市",
+                text = "台北市"),
+                MessageAction(
+                label = "台北市",
+                text = "台北市")
+            ])
+        ]))
+        line_bot_api.reply_message(event.reply_token, carousel_message)
+        
 import os
 if __name__ == "__main__":
     port = int(os.environ.get('PORT', 5000))
