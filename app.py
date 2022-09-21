0285ba5f64fcd6a653673844c9a3bb918d573cc3
DTsai2 <50611692+DTsai2@users.noreply.github.com>
2022-09-21 16:28:34 +0800

    Update app.py

--- app.py
+++ app.py
@@ -205,7 +205,7 @@ def handle_message(event):
         )
         line_bot_api.reply_message(event.reply_token, quick_message)
     
-    print(event.message)
+    print(event)
 import os
 if __name__ == "__main__":
     port = int(os.environ.get('PORT', 5000))
