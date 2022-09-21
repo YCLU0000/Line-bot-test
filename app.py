57a2b2e665f28ffbdfbb2f5b0e65313b7462cf5b
DTsai2 <50611692+DTsai2@users.noreply.github.com>
2022-09-21 21:02:18 +0800

    Update app.py

--- app.py
+++ app.py
@@ -84,7 +84,10 @@ def callback():
         abort(400)
     return 'OK'
         
-
+# 處理位置資訊
+@handler.add(MessageEvent, message=LocationMessage)
+def handle_message(event):
+    print(event)
 # 處理訊息
 @handler.add(MessageEvent, message=TextMessage)
 def handle_message(event):
